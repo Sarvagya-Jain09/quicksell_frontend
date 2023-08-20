9b9b3fa0e4ae1aed8e0773b3258876f9a18e1e08
MihirBibhuty <bibhutymihir@gmail.com>
2023-08-20 09:31:21 +0530

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import ThemeContext from './context/ThemeContext';
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Board from "./components/Board/Board";
